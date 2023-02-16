753eeb3b8b0f16ce9132aa4e34f82b93a1bc1aaa
DemonVenom <xxspamxxemailxx@gmail.com>
2023-02-16 17:39:29 -0500

    Linked server dir to pages

--- index.js
+++ index.js
@@ -1,35 +1,52 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 8080;
 
 const index = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Type', 'text/html');
   res.setHeader('Access-Control-Allow-Origin','*');
 
 
   const{method, url} = req;
 
-  if(url === '/index' && method === 'GET') {
-    res.write(`Here is a index.\n`);
-    res.write(`Method:${method}, URL:${url}`);
-    res.end();
+  if(url === '/') {
+
+    fs.readFile("index.html", function(err, text){
+      res.end(text);
+    });
+
+    return;
+
   }
   else if(url === '/about') {
-    res.write(`Here is a about.\n`);
-    res.write(`Method:${method}, URL:${url}`);
-    res.end();
+
+    fs.readFile("about.html", function(err, text){
+      res.end(text);
+    });
+
+    return;
+
   }
   else if(url === '/contact-me') {
-    res.write(`Here is a contact.\n`);
-    res.write(`Method:${method}, URL:${url}`);
-    res.end();
+
+    fs.readFile("contact-me.html", function(err, text){
+      res.end(text);
+    });
+
+    return;
+
   }
   else {
-    res.write(`I don't recognize that request.\n`);
-    res.write(`Method:${method}, URL:${url}`);
-    res.end();
+
+    fs.readFile("404.html", function(err, text){
+      res.end(text);
+    });
+
+    return;
+
   }
 
 
